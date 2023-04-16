bb3fd1d9f0e3766665da33467cf17d73aec9d9cc
crichardd <crichard11@myges.fr>
2023-04-16 16:37:41 +0200

    ajout de boostrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <nav class="navbar navbar-expand-sm bg-light navbar-dark">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">
+                <img src="logo.png" alt="Avatar Logo" style={{width: '40px'}} className="rounded-pill"/>               
+              </a>
+            </div>
+          </nav>
       </header>
     </div>
   );
