a5c3369ae504cc56f7d067fcfff77e2f03e4fc43
crichardd <crichard11@myges.fr>
2023-04-20 17:38:45 +0200

    reageancement des pages

--- src/profil.tsx
+++ src/profil.tsx
@@ -4,24 +4,7 @@ import { useState} from 'react';
 
 
 function Profil() {
-  const [pseudo, setPseudo] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(`Pseudo: ${pseudo}, Email: ${email}, Mot de passe: ${password}`);
-  }
-
-  //animation signIn signUp
-  const switchers = [...document.querySelectorAll('.switcher')]
-
-  switchers.forEach(item => {
-    item.addEventListener('click', function() {
-      switchers.forEach(item => item.parentElement.classList.remove('is-active'))
-      this.parentElement.classList.add('is-active')
-    })
-  })
+  
 
   return (
     <div className="App">
