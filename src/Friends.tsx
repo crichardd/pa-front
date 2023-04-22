bc3d2d7105856a0baa0004e4ef72bfdc1ffcccd7
crichardd <crichard11@myges.fr>
2023-04-22 08:58:35 +0200

    creation des page manquante

---
+++ src/Friends.tsx
@@ -0,0 +1,15 @@
+function Friends() {
+  
+
+    return (
+        <div className="App">
+        <header className="App-header bg-light">
+            <div className='mint titleMain'><h1>Stime</h1> </div>
+        </header>
+        <main className='LandingPage-main'></main>
+        </div>
+    );
+}
+
+export default Friends;
+
---
+++ src/Message.tsx
@@ -0,0 +1,14 @@
+function Message() {
+  
+
+    return (
+        <div className="App">
+        <header className="App-header bg-light">
+            <div className='mint titleMain'><h1>Stime</h1> </div>
+        </header>
+        <main className='LandingPage-main'></main>
+        </div>
+    );
+}
+
+export default Message;
